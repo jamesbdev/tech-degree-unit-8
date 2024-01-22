3cae01eb1eaa85da8e544589f0ac9e5eefcf7a83
James Bremner <j.bremner@ymail.com>
2024-01-22 11:42:20 +0000

    add fetch request

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
-
 //make fetch request
 
-
-
 async function logEmployees() {
-    const response = await fetch("https://randomuser.me/api/");
+    const response = await fetch("https://randomuser.me/api/?results=12&nat=us");
     const employeesData = await response.json();
-    console.log(employeesData.results[0]);
+    const results = employeesData.results;
+    
+    console.log(results);
+    results.forEach(employee => {
+        const mainContainer = document.querySelector('.group-container');
+        const employeeCard = document.createElement('div');
+        employeeCard.classList.add('employee-card');
+        employeeCard.innerHTML = `<img src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}" class="employee-image">
+        <h2 class="employee-name">${employee.name.first} ${employee.name.last}</h2>`;
+    
+        mainContainer.appendChild(employeeCard);
+    });
+
   }
 
 logEmployees();
+
+//add employees to the page
+
